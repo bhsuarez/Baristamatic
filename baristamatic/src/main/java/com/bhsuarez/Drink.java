c25f1afb3694f680420bcb0deb188b7219cba0c9
bhsuarez <bhsuarez@me.com>
2018-04-14 18:27:19 -0500

    Updated Drink.java and JUnit tests

--- baristamatic/src/main/java/com/bhsuarez/Drink.java
+++ baristamatic/src/main/java/com/bhsuarez/Drink.java
@@ -2,6 +2,19 @@ package com.bhsuarez;
 
 public class Drink {
 
+    // Define menu number
+    private int menu;
+
+    // Define drink name
     private String drinkName;
-    private double drinkPrice;
+
+    // Define array of drink ingredients
+    private Ingredient[] ingredients = null;
+
+    // Default constructor
+    public Drink(int menu, String drinkName, Ingredient[] ingredients){
+        this.menu = menu;
+        this.drinkName = drinkName;
+        this.ingredients = ingredients;
+    }
 }
--- baristamatic/src/test/java/com/bhsuarez/DrinkTest.java
+++ baristamatic/src/test/java/com/bhsuarez/DrinkTest.java
@@ -1,4 +1,15 @@
 package com.bhsuarez;
 
+import org.junit.Before;
+
 public class DrinkTest {
+
+    Drink drink = null;
+
+    @Before
+    public void setUpDrink(){
+        drink = new Drink(0,"Drink Name", new Ingredient[]{
+                new Ingredient("Name",1,10)});
+    }
+
 }
