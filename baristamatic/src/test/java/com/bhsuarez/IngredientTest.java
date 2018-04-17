ab3ded93322aec04360577ea399f9f549f7a86fd
bhsuarez <bhsuarez@me.com>
2018-04-17 09:58:29 -0500

    Completed IngredientTest

--- baristamatic/src/test/java/com/bhsuarez/IngredientTest.java
+++ baristamatic/src/test/java/com/bhsuarez/IngredientTest.java
@@ -14,42 +14,62 @@ public class IngredientTest {
         ingredient = new Ingredient(0,"Name",10);
     }
 
-    // Test for setIngredientName()
+    // Test for getIngredientName() is true
     @Test
-    public void testSetIngredientName(){
-        ingredient.setIngredientName("Test");
-        assertTrue("Test",true);
-    }
-
-    //  Test for setIngredientInventoryCount()
-    @Test
-    public void testSetIngredientInventoryCount(){
-        ingredient.setIngredientInventoryCount(10);
-        assertEquals(ingredient.getIngredientInventoryCount(),10,0);
-    }
-
-    // Test for getIngredientName()
-    @Test
-    public void testGetIngredientName(){
+    public void testGetIngredientName_True(){
         assertTrue(ingredient.getIngredientName(),true);
+    }
+
+    // Test for getIngredientName() is false
+    @Test
+    public void testGetIngredientName_False(){
+        assertFalse(ingredient.getIngredientName(),false);
+    }
+
+    // Test for setIngredientName() is null
+    @Test
+    public void testSetIngredientName_Null(){
+        ingredient.setIngredientName(null);
+        assertNull(ingredient.getIngredientName());
+    }
+
+    // Test for setIngredientName() is not null
+    @Test
+    public void testSetIngredientName_Empty(){
+        ingredient.setIngredientName("");
+        assertEquals(ingredient.getIngredientName(),"");
+    }
+
+    //  Test for setIngredientInventoryCount() less than zero
+    @Test
+    public void testSetIngredientInventoryCount_Less_Than_Zero(){
+        ingredient.setIngredientInventoryCount(-1);
+        assertTrue(ingredient.getIngredientInventoryCount()<0);
+    }
+
+    // Test for setIngredientInventoryCount greater than 10
+    @Test
+    public void testSetIngredientInventoryCount_Greater_Than_Ten(){
+        ingredient.setIngredientInventoryCount(11);
+        assertTrue(ingredient.getIngredientInventoryCount()>10);
     }
 
     // Test for getIngredientInventoryCount()
     @Test
-    public void testGetIngredientInventoryCount(){
-        assertEquals(ingredient.getIngredientInventoryCount(),10,0);
+    public void testGetIngredientInventoryCount_True(){
+        assertEquals(ingredient.getIngredientInventoryCount(),20,10);
     }
 
-    // Test for useIngredient()
+    // Test for useIngredient() more than getIngredientInventoryCount value
     @Test
-    public void testUseIngredient_False(){
-        ingredient.useIngredient(1);
-        assertFalse(ingredient.getIngredientInventoryCount()<0);
+    public void testUseIngredient_More_Than_InventoryCount(){
+        ingredient.useIngredient(ingredient.getIngredientInventoryCount());
+        assertEquals(ingredient.getIngredientInventoryCount(),0);
     }
 
     // Test for reloadIngredientCount
     @Test
-    public void testReloadIngredientCount(){
+    public void testReloadIngredientCount_True(){
         ingredient.reloadIngredientCount();
         assertEquals(ingredient.getIngredientInventoryCount(),10,0);
     }
